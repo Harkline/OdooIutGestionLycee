f0d67c3c723e6580922887c59f3082afd9693120
Harkline <yanislevesque@hotmail.fr>
2021-05-09 19:43:29 +0200

    Calcule désormais l'age de l'élève en se basant sur sa date de naissance à la création de celui-ci

--- models/Eleve.py
+++ models/Eleve.py
@@ -11,8 +11,19 @@ class Eleve(models.Model):
 
     class_id = fields.Many2one("classe")
 
-    #A définir, calcul de l'age à partir de sa date de naissance
+    #Calcul de l'age à partir de sa date de naissance
     @api.depends('birthdate')
     def compute_age_student(self):
+        #On récupère la date d'aujourd'hui
         todayDate = date.today()
-        self.age = todayDate.year - self.birthdate.year - ((todayDate.month, todayDate.day) < (self.birthdate.month, self.birthdate.day))
+        #On vérifie que la date de naissance n'est pas vide
+        if (self.birthdate != 0):
+            #On calcule la différence en année
+            #La différence des deux dates est un "timedelta" : https://docs.python.org/3/library/datetime.html
+            #On utilise l'un des champs possibles de timedelta (Les jours) pour retrouver le nombre d'années de différence (donc l'age)
+            self.age = (todayDate - self.birthdate).days/365
+            
+
+            
+        
+        #self.age = todayDate.year - self.birthdate.year - ((todayDate.month, todayDate.day) < (self.birthdate.month, self.birthdate.day))
